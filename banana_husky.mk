0228c506c317f2325503a35605132714f781abf6
Red Prez <redprezstuff@gmail.com>
2024-01-26 18:14:01 -0600

    Derp stuff to Banana

--- banana_husky.mk
+++ banana_husky.mk
@@ -6,7 +6,7 @@
 
 # Inherit some common Lineage stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/banana/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/shusky/aosp_husky.mk)
@@ -14,10 +14,19 @@ $(call inherit-product, device/google/zuma/lineage_common.mk)
 
 include device/google/shusky/husky/device-lineage.mk
 
+
+BANANA_BUILD_TYPE := OFFICIAL
+BANANA_MAINTAINER := Red_Prez16
+TARGET_EXCLUDE_MATLOG := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+WITH_GAPPS := true
+TARGET_BOOT_ANIMATION_RES := 1440p
+
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 8 Pro
-PRODUCT_NAME := derp_husky
+PRODUCT_NAME := banana_husky
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2992
